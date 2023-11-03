125a2cb2f38985fb14b3307da2997ada652e209e
Gaute Rønningen <gaute.ronningen@nb.no>
2023-11-03 14:46:34 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,83 @@
 <script setup lang="ts">
-import { useDataStore } from '@/stores/Data';
+import { useDataStore } from '@/stores/data';
 
 const store = useDataStore();
-store.getHomeId().then((HomeId: string) => {
-  store.getWebsocketSubscriptionUrl().then((FeedApiUrl: string) => {
-    store.subscribeToLiveMeasurement(HomeId, FeedApiUrl);
+store.getWebsocketSubscriptionUrl().then((FeedApiUrl: string) => {
+  store.getHome().then(() => {
+    store.subscribeToLiveMeasurement(FeedApiUrl);
   });
 });
+
+const getTimeLastAboveThreshold = function(): string {
+  const lastAboveThreshold = store.getLastTimestampAboveThreshold(3500);
+  if (lastAboveThreshold == null) {
+    return 'Ukjent';
+  }
+  return getTextualSince(lastAboveThreshold.getTime());
+}
+
+const getTextualSince = function(timestamp: number): string {
+  if (timestamp == null) {
+    return 'Ukjent';
+  }
+  const now = new Date().getTime();
+  const diff = now - timestamp;
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
+
+  if (seconds < 60) {
+    return `${seconds} sekunder siden`;
+  } else if (minutes < 60) {
+    return `${minutes} minutter siden`;
+  } else if (hours < 24) {
+    return `${hours} timer siden`;
+  } else if (days < 7) {
+    return `${days} dager siden`;
+  } else if (weeks < 4) {
+    return `${weeks} uker siden`;
+  } else if (months < 12) {
+    return `${months} måneder siden`;
+  } else {
+    return `${years} år siden`;
+  }
+}
 </script>
 
 <template>
   <div>
     <header>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">{{ store.home.address.address1 }}, {{ store.home.address.city }}</span>
+        </div>
+      </nav>
     </header>
     <main>
-
+      <div class="row">
+        <div class="card" style="width:30%;margin:1rem;">
+          <div class="card-body">
+            <h5 class="card-title">Siste avlesning</h5>
+            <p class="card-text">{{ store.getLatestPower() }}</p>
+          </div>
+        </div>
+        <div class="card" style="width:30%;margin:1rem;">
+          <div class="card-body">
+            <h5 class="card-title">Varmepumpa startet sist</h5>
+            <p class="card-text">{{ getTimeLastAboveThreshold() }}</p>
+          </div>
+        </div>
+        <div class="card" style="width:30%;margin:1rem;">
+          <div class="card-body">
+            <h5 class="card-title">Klokka er</h5>
+            <p class="card-text">{{ new Date().toLocaleTimeString() }}</p>
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
--- src/stores/data.ts
+++ src/stores/data.ts
@@ -1,9 +1,9 @@
 import { defineStore } from 'pinia'
 import Axios from 'axios'
-import { createClient, type ExecutionResult } from 'graphql-ws';
+import { v4 } from 'uuid'
 
 export interface DataState {
-    homeId: string;
+    homeId: Home;
     log: LogItem[]
 }
 
@@ -12,8 +12,40 @@ export interface LogItem {
     power: number;
 }
 
+export interface Home {
+    id: string;
+    address: Address;
+}
+
+export interface Address {
+    address1: string;
+    postalCode: string;
+    city: string;
+    country: string;
+    latitude: number;
+    longitude: number;
+}
+
 export const useDataStore = defineStore('data', {
-    state: () => ({ homeId: "", log: [{ timestamp: "", power: 0.00}]}),
+    state: () => ({ 
+        home: {
+            id: "",
+            address: { 
+                address1: "", 
+                postalCode: "", 
+                city: "", 
+                country: "", 
+                latitude: 0,
+                longtitude: 0
+            }
+        }, 
+        log: [
+            {
+                timestamp: "",
+                power: 0
+            }
+        ]
+    }),
     getters: {
         getApiKey(): string {
             return import.meta.env.VITE_TIBBER_API_KEY || ""
@@ -24,19 +56,19 @@ export const useDataStore = defineStore('data', {
     },
     actions: {
         /**
-         * Populates the home ID by making a request to the Tibber API.
-         * @returns A Promise that resolves when the home ID has been successfully populated.
+         * Retrieves the home for the current user from the Tibber API.
+         * @returns A Promise that resolves to a Home object.
          */
-        async getHomeId(): Promise<string> {
+        async getHome(): Promise<Home> {
             const apiUrl = this.getQueryApiUrl
-            const homeIdQuery = `{viewer {homes {id}}}`
+            const homeIdQuery = `{viewer {homes {id address {address1 postalCode city country latitude longitude }}}}`
             const apiKey = this.getApiKey
-            
+
             const request = await Axios.post(apiUrl, { query: homeIdQuery }, { headers: { Authorization: `Bearer ${apiKey}` } })
                 .then(response => {
-                    const homeId = response.data.data.viewer.homes[0].id
-                    this.homeId = homeId
-                    return homeId
+                    const home = response.data.data.viewer.homes[0]
+                    this.home = home
+                    return home
                 })
                 .catch(error => console.log(error))
             return request
@@ -47,9 +79,9 @@ export const useDataStore = defineStore('data', {
          */
         async getWebsocketSubscriptionUrl(): Promise<string> {
             const apiUrl = this.getQueryApiUrl
-            const wsSubUrlQuery =`{viewer { websocketSubscriptionUrl}}`
+            const wsSubUrlQuery = `{viewer { websocketSubscriptionUrl}}`
             const apiKey = this.getApiKey
-            
+
             const request = await Axios.post(apiUrl, { query: wsSubUrlQuery }, { headers: { Authorization: `Bearer ${apiKey}` } })
                 .then(response => {
                     return response.data.data.viewer.websocketSubscriptionUrl
@@ -62,39 +94,56 @@ export const useDataStore = defineStore('data', {
          * @param homeId The home ID to subscribe to.
          * @returns void
          */
-        async subscribeToLiveMeasurement(homeId: string, apiUrl: string) {
+        async subscribeToLiveMeasurement(apiUrl: string) {
+            const me = this
             const apiKey = this.getApiKey
 
-            const client = createClient({
-                url: apiUrl,
-                lazy: false,
-                connectionParams: () => ({
-                    headers: {
-                        Authorization: `Bearer ${apiKey}`,
-                    },
-                }),
+            // Create a new websocket connection
+            const ws = new WebSocket(apiUrl, "graphql-transport-ws")
+            ws.binaryType = "arraybuffer"
+
+            ws.addEventListener("open", () => {
+                ws.send(JSON.stringify({
+                    type: "connection_init",
+                    payload: {
+                        token: apiKey
+                    }
+                }))
             })
 
-            const subscription = `subscription {
-                liveMeasurement(homeId: "${homeId}") {
-                    timestamp
-                    power
+            // Handle incoming messages
+            ws.addEventListener("message", (event) => {
+                const data = JSON.parse(event.data)
+                if (data.type === "next") {
+                    const measurement: LogItem = data.payload.data.liveMeasurement
+                    me.appendLogItem(measurement)
+                } else if (data.type === "connection_ack") {
+                    // When the server acknowledges the connection, send the subscription request
+                    ws.send(JSON.stringify({
+                        id: v4(),
+                        type: "subscribe",
+                        payload: {
+                            query: `subscription {
+                                liveMeasurement(homeId: "${me.home.id}") {
+                                    timestamp
+                                    power
+                                }
+                            }`,
+                            variables: {},
+                            extensions: {}
+                        }
+                    }))
                 }
-            }`
+            })
 
-            client.subscribe({ query: subscription }, {
-                next: (data: ExecutionResult<{ liveMeasurement: { timestamp: string, power: number } }, Record<string, unknown>>) => {
-                    const measurement = data?.data?.liveMeasurement;
-                    if (measurement) {
-                        this.log.push(measurement);
-                    }
-                },
-                error: (error: unknown) => {
-                    console.log(error);
-                },
-                complete: () => {
-                    console.log('Subscription completed');
-                }
+            // Handle errors
+            ws.addEventListener("error", (event) => {
+                console.log(event)
+            })
+
+            // Handle completion
+            ws.addEventListener("close", () => {
+                console.log("Websocket connection closed.")
             })
         },
         /**
@@ -103,6 +152,14 @@ export const useDataStore = defineStore('data', {
          */
         getLog(): LogItem[] {
             return this.log
+        },
+        /**
+         * Appends a LogItem object to the log.
+         * @param {LogItem} item The LogItem object to append to the log.
+         * @returns void
+         */
+        appendLogItem(item: LogItem): void {
+            this.log.push(item)
         },
         /**
          * Returns the latest measurement from the log.
@@ -117,6 +174,20 @@ export const useDataStore = defineStore('data', {
          */
         getLatestPower(): number {
             return this.getLatestMeasurement().power
+        },
+        /**
+         * Get last timestamp the power was above a certain threshold
+         * @param threshold The threshold to check against.
+         * @returns The last timestamp the power was above the threshold.
+         */
+        getLastTimestampAboveThreshold(threshold: number): Date | null {
+            const log = this.getLog()
+            const lastItem = log.slice().reverse().find(item => item.power > threshold)
+            if (lastItem) {
+                return new Date(lastItem.timestamp)
+            } else {
+                return null
+            }
         }
     }
 })
